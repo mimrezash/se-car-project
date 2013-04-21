5de9689dedf5f3b5742f8e9fb79c255cd68d7102
nightboy201@gmail.com <nightboy201@gmail.com@2c7346a7-b0a3-ce06-ca90-0743ababafde>
2013-04-21 20:02:25 +0000

    Tests to be added

---
+++ src/hybrid/car/test/BreakTest.java
@@ -0,0 +1,14 @@
+package hybrid.car.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BreakTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
