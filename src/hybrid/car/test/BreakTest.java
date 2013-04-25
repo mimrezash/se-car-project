d0eb2ea85edf26ece32cdf88cfd97b3ea7ba5ce0
nightboy201@gmail.com <nightboy201@gmail.com@2c7346a7-b0a3-ce06-ca90-0743ababafde>
2013-04-25 17:09:54 +0000

    jtest added

--- src/hybrid/car/test/BreakTest.java
+++ src/hybrid/car/test/BreakTest.java
@@ -1,6 +1,7 @@
 package hybrid.car.test;
 
 import static org.junit.Assert.*;
+import hybrid.car.system.Breaks;
 
 import org.junit.Test;
 
@@ -8,6 +9,11 @@ public class BreakTest {
 
 	@Test
 	public void test() {
+		Breaks bk = new Breaks();
+		assertEquals(bk.getName(),"Breaks" );
+		assertTrue(bk.getUniqueId() == -1);
+		assertFalse(bk.getUniqueId() > -1);		
+		
 		fail("Not yet implemented");
 	}
 
